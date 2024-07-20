3ef6df91870c0a13abbe49b169f92ddd1b47ce35
wittawatNa <wittawat.naraong@gmail.com>
2024-07-20 21:55:35 +0700

    first commit not pass 2 case

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
 import React, { useEffect } from 'react';
 import * as d3 from 'd3';
 import './App.css';
+/*
+const strTimeCvt =(str)=>{
+    // Split the time string into minutes and seconds
+    const [minutes, seconds] = str.split(':').map(Number);
 
+    // Calculate the total seconds
+    const totalSeconds = (minutes * 60) + seconds;
+  
+    return totalSeconds;
+}
+*/
 const App = () => {
   useEffect(() => {
 
-    d3.json('https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/GDP-data.json')
+    d3.json('https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/cyclist-data.json')
       .then(data => {
-        if (!data||!data.data) {
+        if (!data) {
           console.error('No data found');
           return;
         }
         console.log("data: " + JSON.stringify(data, null, 2));
-        console.log("data length: " + data.data.length);
+        console.log("data length: " + data.length);
 
         const width = 1000;
         const height = 500;
-        const barWidth = width / data.data.length;
 
         // Clear previous SVG elements to prevent duplicate graphs
         d3.select('.visHolder').selectAll('*').remove();
@@ -28,57 +37,66 @@ const App = () => {
       let svgContainer = d3.select('.visHolder')
         .append('svg')
         .attr('width', width + 120)
-        .attr('height', height + 100);//height 50 for offset level up graph and 50 for space on top
+        .attr('height', height + 100);
+        const X = data.map(item => Number(item.Year));
+        //console.log("X:"+dateData);
+        const maxX = (d3.max(X));
+        let minX = (d3.min(X));
+        minX--;
+        //console.log("X max:min "+maxX+":"+minX);
 
-        const dateData = data.data.map(item => new Date(item[0]));
-        const maxX = new Date(d3.max(dateData));
-        const minX = new Date(d3.min(dateData));
-
-        const xScale = d3.scaleTime()
+        const xScale = d3.scaleLinear()
           .domain([minX, maxX])
           .range([0, width]);
-        const xAxis = d3.axisBottom().scale(xScale);
+        const xAxis = d3.axisBottom(xScale).tickFormat(d3.format('d'));
 
         svgContainer.append('g')
           .call(xAxis)
           .attr('id', 'x-axis')
-          .attr('transform', 'translate(60,'+ (height+50) +')');
+          .attr('transform', 'translate(60,'+ (height+20) +')');
 
-        const Y = data.data.map(item => item[1]);
+      
+        const Y = data.map(item => {
+          const [minutes, seconds]=(item.Time).split(':')
+          return new Date(1970, 0, 1, 0, minutes, seconds);
+      });
         const maxY = d3.max(Y);
-        const yScaleRv = d3.scaleLinear()
-          .domain([0, maxY])
-          .range([height, 0]);
+        const minY = d3.min(Y);
+        const timeFormat = d3.timeFormat('%M:%S');
+        //console.log("Y:"+Y);
+        //console.log("Y max:min "+maxY+":"+minY);
 
-        const yScale = d3.scaleLinear()
-          .domain([0, maxY])
+
+        const yScale = d3.scaleTime()
+          .domain([minY, maxY])
           .range([0, height]);
 
-        const yAxis = d3.axisLeft(yScaleRv);
+        const yAxis = d3.axisLeft(yScale).tickFormat(timeFormat);
         svgContainer.append('g')
           .call(yAxis)
           .attr('id', 'y-axis')
-          .attr('transform', 'translate(60, 50)');
+          .attr('transform', 'translate(60, 20)');
 
-        svgContainer.selectAll('rect')
-          .data(Y)
+        //merge data
+        const mData = X.map((value, index) => [value, Y[index]]);
+
+        svgContainer.selectAll('circle')
+          .data(mData)
           .enter()
-          .append('rect')
-          .attr('class', 'bar')
+          .append('circle')
+          .attr('class', 'dot')
           .attr('index', (d, i) => i)
-          .attr('data-date', (d, i) => data.data[i][0])
-          .attr('data-gdp', (d, i) => data.data[i][1])
-          .attr('x', (d, i) => xScale(dateData[i]))
-          .attr('y', d => height-yScale(d))
-          .attr('width', barWidth)
-          .attr('height', d => yScale(d))
-          .style('fill', '#ddf')
-          .attr('transform', 'translate(60, 50)')
+          .attr('data-xvalue', (d, i) => d[0])
+          .attr('data-yvalue', (d, i) => d[1])
+          .attr('cx', (d, i) => xScale(d[0]))
+          .attr('cy', (d, i) => yScale(d[1]))
+          .attr('r', 10)
+          .style('fill', '#233')
+          .attr('transform', 'translate(60, 20)')
           .on('mouseover', function (event, d) {
             const i = this.getAttribute('index');
             tooltip.transition().duration(200).style('opacity', 0.9);
-            tooltip.html(dateData[i] + '<br>$' + Y[i])
-              .attr('data-date', data.data[i][0]).style('left', i * barWidth + 40 + 'px');
+            tooltip.html(data[i].Name + '<br>Time:' + data[i].Time + '<br>Year:' + data[i].Year).style('left', 40 + 'px');
           })
           .on('mouseout', function () {
             tooltip.transition().duration(300).style('opacity', 0);
@@ -90,7 +108,7 @@ const App = () => {
   return (
     <div className="App">
       <div className="container">
-        <div id="title">United States GDP</div>
+        <div id="title">Doping in Professional Bicycle Racing</div>
         <div className="visHolder"></div>
       </div>
     </div>
